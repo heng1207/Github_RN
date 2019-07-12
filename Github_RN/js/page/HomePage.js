3999f40e6413444bfcc168e7131d208cafbe31f5
Your zhangheng <1356770740.com>
2019-07-12 17:48:20 +0800

    7月12日
    基于react-navigation的APP导航框架搭建-底部导航

--- Github_RN/js/page/HomePage.js
+++ Github_RN/js/page/HomePage.js
@@ -1,18 +1,87 @@
 
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View,Image} from 'react-native';
+import {createStackNavigator,
+    createBottomTabNavigator,
+    createAppContainer,
+    createSwitchNavigator,
+    createMaterialTopTabNavigator
+} from "react-navigation"
 
+import  PopularPage from "./PopularPage"
+import  TrandingPage from "./TrandingPage"
+import  FavoritePage from "./FavoritePage"
+import  MyPage from "./MyPage"
+import  {Images} from "../images";
 
 
 type Props = {};
 export default class HomePage extends Component<Props> {
+
+    _tabNavigator() {
+
+        return createBottomTabNavigator({
+
+            PopularPage:{
+                screen :PopularPage,
+                navigationOptions:{
+                    tabBarLabel : "最热",
+                    tabBarIcon: ({ focused, tintColor }) => (
+                        <Image
+                            source={focused ?  Images.Tab.OneActive : Images.Tab.One}
+                            style={{ width: 25, height: 25 }}
+                        />
+                    )
+
+                }
+            },
+            TrandingPage:{
+                screen :TrandingPage,
+                navigationOptions:{
+                    tabBarLabel : "趋势",
+                    tabBarIcon: ({ focused, tintColor }) => (
+                        <Image
+                            source={focused ?  Images.Tab.TwoActive : Images.Tab.Two}
+                            style={{ width: 25, height: 25 }}
+                        />
+                    )
+                }
+            },
+            FavoritePage:{
+                screen :FavoritePage,
+                navigationOptions:{
+                    tabBarLabel : "收藏",
+                    tabBarIcon: ({ focused, tintColor }) => (
+                        <Image
+                            source={focused ?  Images.Tab.OneActive : Images.Tab.One}
+                            style={{ width: 25, height: 25 }}
+                        />
+                    )
+                }
+            },
+            MyPage:{
+                screen :MyPage,
+                navigationOptions:{
+                    tabBarLabel : "我的",
+                    tabBarIcon: ({ focused, tintColor }) => (
+                        <Image
+                            source={focused ?  Images.Tab.TwoActive : Images.Tab.Two}
+                            style={{ width: 25, height: 25 }}
+                        />
+                    )
+                }
+            }
+        });
+
+    }
+
+
+
     render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>HomePage !</Text>
-            </View>
-        );
+        const  Tab = this._tabNavigator();
+        const AppContainer = createAppContainer(Tab);
+        return  <AppContainer/>
     }
 }
 
